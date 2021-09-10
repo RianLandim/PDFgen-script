9235afb9ae77e737911589c669fc895e9bde24bc
Juan Dantas <rianlandim22@gmail.com>
2021-09-10 13:57:48 -0300

    refactor: script without server

--- src/server.js
+++ src/server.js
@@ -1,41 +1,20 @@
 const ejs = require("ejs");
 const path = require("path");
-const express = require("express");
-const puppeteer = require("puppeteer");
+const pdf = require("html-pdf");
 const passengers = require("./util/const");
 
-const app = express();
+const options = { format: "letter" };
 
-app.get("/pdf", async (req, res) => {
-
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  await page.goto("http://localhost:3000/", {
-    waitUntil: "networkidle0"
-  });
-
-  const pdf = await page.pdf({
-    printBackground: true,
-    format: "letter"
-  });
-
-  await browser.close();
-
-  res.contentType("application/pdf");
-
-  return res.send(pdf);
-
-});
-
-app.get("/", (req, res) => {
+passengers.forEach((passengers) => {
   const filePath = path.join(__dirname, "screen", "template.ejs");
-  ejs.renderFile(filePath, { passengers }, (err, html) => {
-    if (err) return res.send("Erro na leitura de arquivo!");
-    return res.send(html);
-  });
-});
+  const pdfPath = path.join(__dirname, "pdf", "/");
 
-app.listen(3000, () => {
-  console.log("Server Running!");
+  ejs.renderFile(filePath, { passengers }, (err, html) => {
+    if (err) return console.log(err);
+    pdf.create(html, options)
+      .toFile(pdfPath + passengers.name + ".pdf", function (err, res) {
+        if (err) return console.log("Erro ao gerar o PDF!");
+        console.log(res);
+      });
+  });
 });
