760125c76dbb1ccd22ebee256940fb93c2f62e91
Juan Dantas <rianlandim22@gmail.com>
2021-09-10 11:01:57 -0300

    chore: add puppeteer

--- src/server.js
+++ src/server.js
@@ -1,13 +1,4 @@
 const pdf = require("html-pdf");
 const ejs = require("ejs");
+const path = require("path");
 
-const name = "Rian Landim";
-const email = "rianlandim222@gmail.com";
-
-ejs.renderFile("./src/screen/template.ejs", { name, email }, (err, html) => {
-  if (err) console.log(err);
-  pdf.create(html, {}).toFile("./pdf´s/template.pdf", (err, res) => {
-    if (err) return console.log(err);
-    console.log(res);
-  });
-});
