7777f7dc3c64b43b126ad725dd6d2db24914a49f
Juan Dantas <rianlandim22@gmail.com>
2021-09-10 10:39:43 -0300

    chore: add eslint config file

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const pdf = require("html-pdf");
 const ejs = require("ejs");
 
-let name = "Rian Landim";
-let email = "rianlandim222@gmail.com";
+const name = "Rian Landim";
+const email = "rianlandim222@gmail.com";
 
 ejs.renderFile("./src/screen/template.ejs", { name, email }, (err, html) => {
   if (err) console.log(err);
-  pdf.create(html, {}).toFile("./pdf´s/template.pdf", function (err, res) {
+  pdf.create(html, {}).toFile("./pdf´s/template.pdf", (err, res) => {
     if (err) return console.log(err);
     console.log(res);
   });
