19cb9fa9c92a67a2005632218020f3b2f38a53b0
Juan Dantas <rianlandim22@gmail.com>
2021-09-10 11:30:37 -0300

    feat: puppeteer pdf generation

--- src/server.js
+++ src/server.js
@@ -1,4 +1,41 @@
-const pdf = require("html-pdf");
 const ejs = require("ejs");
 const path = require("path");
+const express = require("express");
+const puppeteer = require("puppeteer");
+const passengers = require("./util/const");
 
+const app = express();
+
+app.get("/pdf", async (req, res) => {
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto("http://localhost:3000/", {
+    waitUntil: "networkidle0"
+  });
+
+  const pdf = await page.pdf({
+    printBackground: true,
+    format: "letter"
+  });
+
+  await browser.close();
+
+  res.contentType("application/pdf");
+
+  return res.send(pdf);
+
+});
+
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "screen", "template.ejs");
+  ejs.renderFile(filePath, { passengers }, (err, html) => {
+    if (err) return res.send("Erro na leitura de arquivo!");
+    return res.send(html);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server Running!");
+});
